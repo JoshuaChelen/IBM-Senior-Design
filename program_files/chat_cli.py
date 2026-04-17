f7cd74209d402ea368da7d0dc1d82d9d46d20f16
TristanDend <tdend413@gmail.com>
2026-04-17 17:14:27 -0400

    Draft of chat cli

--- program_files/chat_cli.py
+++ program_files/chat_cli.py
@@ -2,3 +2,31 @@
 Main user interface for the terminal chatbot (connect to nlip_web if time permits)
 - handles conversation, follow-ups, and what-if scenarios
 """
+
+from . import ollama_input, data_conversion, data_generator, analyzer, config, user_input
+
+def conversation():
+    print("""
+    Hello, I can estimate the queue values if you describe to me what your system is and what components it includes.
+    Please include what you know about your system, such as the following information for each component:
+        1. Name of components.
+        2. Types of components (service, database, connection, etc.).
+        3. Operating system of components.
+        4. Description of components.
+        5. Delay in seconds.
+        6. Network speed in mbps.
+        7. Message input and output types.
+        8. Message size in bytes.
+        9. How the components are connected (call, read, write, etc.).
+        10. How often the components are used (i.e. probablity of going to each component).
+        11. Any other relevant information, such as system version.
+        \n
+    """)
+
+    system_description, system_description_path = ollama_input.ask_sys_desc()
+
+    queue_network = data_conversion.system_to_queue(str(system_description_path))
+
+    queue_data_name = data_generator.run(queue_network)
+
+    analyzer.json_output(queue_data_name)
