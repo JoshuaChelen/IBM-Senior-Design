eefd04a410ac591ff95ef4c5d8f6f3424241194a
Jason Domingo <jayprdom@udel.edu>
2026-04-17 18:10:23 -0400

    Added the first functionality of chat_cli() which is to ask the user for a system description and call the pipeline to analyze it.

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from program_files.debug import debug
+from program_files import chat_cli, debug
 from pathlib import Path
 
 # Use to toggle on and off DEBUG_MODE 
-DEBUG_MODE = True 
+DEBUG_MODE = False 
 
 def main():
-    if DEBUG_MODE == True: 
-        debug()
+    if DEBUG_MODE: 
+        debug.debug()
 
     else: 
         # If debug mode is not on, we call the chat_cli.py file
-        print("Debug mode off")
+        # print("Debug mode off")
+        chat_cli.chat_cli()
 
 if __name__ == '__main__':
     main()
